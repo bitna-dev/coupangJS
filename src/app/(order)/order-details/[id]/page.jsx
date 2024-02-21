9a790890eedc6de69c29939e049077aafcad3a42
bitna-dev <bitna.dev@gmail.com>
2024-02-21 11:53:33 -0800

    app dir created

---
+++ src/app/(order)/order-details/[id]/page.jsx
@@ -0,0 +1,17 @@
+'use client';
+import React from 'react';
+
+const OrderDetails = ({
+    params,
+    searchParams,
+}) => {
+    const {id} = params;
+    const {hello} = searchParams;
+    return (
+        <div>
+            {id} <br /> {hello}
+        </div>
+    );
+};
+
+export default OrderDetails;
